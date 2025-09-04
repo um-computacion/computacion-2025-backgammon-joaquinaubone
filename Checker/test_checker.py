f807526325e93396f630b22b767dde19e8103c16
joaquinaubone <joaquinaubone123@gmail.com>
2025-09-04 19:18:39 -0300

    creacion clase checker con test

---
+++ Checker/test_checker.py
@@ -0,0 +1,16 @@
+import unittest
+from Checker.checker import Ficha
+
+class TestFicha(unittest.TestCase):
+
+    def test_color_blanco(self):
+        ficha = Ficha('B')
+        self.assertEqual(ficha.obtener_color(), 'B')
+
+    def test_color_negro(self):
+        ficha = Ficha('N')
+        self.assertEqual(ficha.obtener_color(), 'N')
+
+
+if __name__ == '__main__':
+    unittest.main()
