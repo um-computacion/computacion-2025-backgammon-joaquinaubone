6d4a81fce02977686f0082caede2fd94e0e136bf
joaquinaubone <joaquinaubone123@gmail.com>
2025-10-06 12:57:16 -0300

    corrige-formato-en-test_checker-según-pylint

--- Checker/test_checker.py
+++ Checker/test_checker.py
@@ -1,13 +1,18 @@
+"""Tests unitarios para la clase Checker."""
+
 import unittest
 from checker.checker import Ficha
 
 class TestFicha(unittest.TestCase):
+    """Contiene los tests para verificar el comportamiento de la clase Checker."""
 
     def test_color_blanco(self):
+        """Verifica que la clase Checker se inicializa correctamente."""
         ficha = Ficha('B')
         self.assertEqual(ficha.obtener_color(), 'B')
 
     def test_color_negro(self):
+        """Verifica el correcto funcionamiento del método principal (si aplica)."""
         ficha = Ficha('N')
         self.assertEqual(ficha.obtener_color(), 'N')
 
